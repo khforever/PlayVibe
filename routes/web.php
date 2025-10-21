d04b89f2486602360da20822df17b7bbede5a737
khforever <59407743+khforever@users.noreply.github.com>
2025-10-21 19:54:36 +0300

    add Dashboard Routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,33 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+
+Route::get('register', function () {
+    return view('dashboard.auth.register');
+});
+
+
+Route::get('login', function () {
+    return view('dashboard.auth.login');
+});
+
+Route::get('recover', function () {
+    return view('dashboard.auth.recoverPassword');
+});
+
+
+
+Route::get('test', function () {
+    return view('dashboard.welcome');
+});
+
+Route::get('form', function () {
+    return view('dashboard.form');
+});
+
+Route::get('table', function () {
+    return view('dashboard.table');
+});
