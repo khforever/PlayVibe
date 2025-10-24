0b71ae253d1db095df0178e8dd47654988317dbe
khforever <59407743+khforever@users.noreply.github.com>
2025-10-24 08:41:20 +0300

    Modify web.php  of  dashboard

---
+++ resources/views/layouts/dashboard/sidebar.blade copy 2.php
@@ -0,0 +1,123 @@
+ <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+    <div class="main-menu-content">
+      <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+
+          <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+
+
+
+
+
+
+
+
+
+
+              <ul class="menu-content">
+            <li class="active"><a class="menu-item" href="dashboard-ecommerce.html" data-i18n="nav.dash.ecommerce">Category</a>
+            </li>
+            <li><a class="menu-item" href="{{ route('dashboard.addCategory') }}" data-i18n="nav.dash.crypto">addCategory</a>
+            </li>
+            <li><a class="menu-item" href="dashboard-sales.html" data-i18n="nav.dash.sales">Sales</a>
+            </li>
+          </ul>
+
+
+
+
+
+
+
+
+
+        </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <li class=" navigation-header">
+          <span data-i18n="nav.category.layouts">Category</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip"
+          data-placement="right" data-original-title="Layouts"></i>
+        </li>
+        <li class=" nav-item"><a href="#"><i class="la la-columns"></i><span class="menu-title" data-i18n="nav.page_layouts.main">Page layouts</span><span class="badge badge badge-pill badge-danger float-right mr-2">New</span></a>
+          <ul class="menu-content">
+            <li><a class="menu-item" href="{{ route('dashboard.addCategory') }}" data-i18n="nav.page_layouts.1_column">addCategory</a>
+            </li>
+            <li><a class="menu-item" href="layout-2-columns.html" data-i18n="nav.page_layouts.2_columns">2 columns</a>
+            </li>
+
+
+
+            </li>
+          </ul>
+        </li>
+
+
+{{--  --}}
+
+
+    <li class=" navigation-header">
+          <span data-i18n="nav.category.layouts">Layouts2</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip"
+          data-placement="right" data-original-title="Layouts"></i>
+        </li>
+        <li class=" nav-item"><a href="#"><i class="la la-columns"></i><span class="menu-title" data-i18n="nav.page_layouts.main">Page layouts</span><span class="badge badge badge-pill badge-danger float-right mr-2">New</span></a>
+          <ul class="menu-content">
+            <li><a class="menu-item" href="layout-1-column.html" data-i18n="nav.page_layouts.1_column">1 column</a>
+            </li>
+            <li><a class="menu-item" href="layout-2-columns.html" data-i18n="nav.page_layouts.2_columns">2 columns</a>
+            </li>
+
+
+
+            </li>
+          </ul>
+        </li>
+
+
+
+{{--  --}}
+
+
+        {{--  --}}
+
+ 
+
+
+
+
+
+
+
+
+{{--  --}}
+        {{-- <li class=" navigation-header">
+          <span data-i18n="nav.category.support">Support</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip"
+          data-placement="right" data-original-title="Support"></i>
+        </li>
+        <li class=" nav-item"><a href="http://support.pixinvent.com/"><i class="la la-support"></i><span class="menu-title" data-i18n="nav.support_raise_support.main">Raise Support</span></a>
+        </li>
+        <li class=" nav-item">
+          <a href="https://pixinvent.com/modern-admin-clean-bootstrap-4-dashboard-html-template/documentation"><i class="la la-text-height"></i>
+            <span class="menu-title" data-i18n="nav.support_documentation.main">Documentation</span>
+          </a>
+        </li> --}}
+
+
+
+
+      </ul>
+    </div>
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-
+//////////////////////////////////////dashboard auth///////////////////////////////////////////////
 
 Route::get('register', function () {
     return view('dashboard.auth.register');
@@ -33,9 +33,108 @@ Route::get('login', function () {
 
 Route::get('recover', function () {
     return view('dashboard.auth.recoverPassword');
+
 });
 
 
+
+////////////////////////////////////////users/////////////////////////////////////////////
+
+
+//show all users
+Route::get('users', function () {
+    return view('dashboard.users');
+});
+
+
+
+
+//add new user
+Route::get('addUser', function () {
+    return view('dashboard.addUser');
+});
+
+
+
+
+
+
+
+//edit user
+Route::get('editUser', function () {
+    return view('dashboard.editUser');
+});
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////categories/////////////////////////////////////////////
+
+
+//show allcategories
+Route::get('categories', function () {
+    return view('dashboard.categories');
+});
+
+
+//add new category  addCategory
+Route::get('addCategory', function () {
+    return view('dashboard.addCategory');
+})->name('dashboard.addCategory');
+
+// edit category
+Route::get('editCategory', function () {
+    return view('dashboard.editCategory');
+});
+
+
+/////////////////////////////////////////subcategories////////////////////////////////////////////
+
+
+//show all subcategories
+Route::get('subCategories', function () {
+    return view('dashboard.subCategories');
+});
+
+//add new category  addSubCategory
+Route::get('addSubCategory', function () {
+    return view('dashboard.addSubCategory');
+});
+
+
+
+// edit subcategory
+Route::get('editSubCategory', function (){
+    return view('dashboard.editSubCategory');
+});
+
+
+///////////////////////////////////////products//////////////////////////////////////////////
+//show all products
+Route::get('products', function () {
+    return view('dashboard.products');
+});
+
+//add new product
+Route::get('addProduct', function () {
+    return view('dashboard.addProduct');
+});
+
+
+// edit product
+Route::get('editProduct', function () {
+    return view('dashboard.editProduct');
+});
+
+
+
+/////////////////////////////////////////just test////////////////////////////////////////////
 
 Route::get('test', function () {
     return view('dashboard.welcome');
@@ -49,35 +148,10 @@ Route::get('table', function () {
     return view('dashboard.table');
 });
 
-
-
-//show allcategories
-Route::get('categories', function () {
-    return view('dashboard.categories');
-});
-
-
-//show all subcategories
-Route::get('subCategories', function () {
-    return view('dashboard.subCategories');
-});
+/////////////////////////////////////////////////////////////////////////////////////
 
 
 
-//show all products
-Route::get('products', function () {
-    return view('dashboard.products');
-});
-
-
-//add new category  addSubCategory
-Route::get('addSubCategory', function () {
-    return view('dashboard.addSubCategory');
-});
-//add new user
-Route::get('addUser', function () {
-    return view('dashboard.addUser');
-});
 
 //add new attributes
 Route::get('addAttribute', function () {
@@ -85,16 +159,7 @@ Route::get('addAttribute', function () {
 });
 
 
-//add new category  addCategory
-Route::get('addCategory', function () {
-    return view('dashboard.addCategory');
-})->name('dashboard.addCategory');
 
-
-//add new product
-Route::get('addProduct', function () {
-    return view('dashboard.addProduct');
-});
 
 
 //add new review
@@ -102,21 +167,6 @@ Route::get('addReview', function () {
     return view('dashboard.addReview');
 });
 
-//show all users
-Route::get('users', function () {
-    return view('dashboard.users');
-});
 
-Route::get('editUser', function () {
-    return view('dashboard.editUser');
-});
 
-// edit category
-Route::get('editCategory', function () {
-    return view('dashboard.editCategory');
-});
 
-// edit subcategory
-Route::get('editSubCategory', function (){
-    return view('dashboard.editSubCategory');
-});
